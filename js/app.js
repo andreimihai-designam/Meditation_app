71202afb3cfe301d24aa2e271d6abe1e6f32c822
Andrei Mihai <68633606+andreimihai-designam@users.noreply.github.com>
2021-04-01 13:09:47 +0300

    Update app.js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,6 @@ const app = () => {
     const song = document.querySelector(".song");
     const play = document.querySelector(".play");
     const outline = document.querySelector(".moving-outline circle");
-    const video = document.querySelector(".vid-container video");
 
     //Sounds
     const sounds = document.querySelectorAll(".sound-picker button");
@@ -21,7 +20,6 @@ const app = () => {
     sounds.forEach(sound => {
         sound.addEventListener("click", function() {
             song.src = this.getAttribute("data-sound");
-            video.src = this.getAttribute("data-video");
             checkPlaying(song);
         });
     });
@@ -35,7 +33,7 @@ const app = () => {
     timeSelect.forEach(option => {
         option.addEventListener("click", function() {
             fakeDuration = this.getAttribute("data-time");
-            timeDisplay.textContent = `0${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}0`;
+            timeDisplay.textContent = `${Math.floor(fakeDuration / 60)}:${Math.floor(fakeDuration % 60)}0`;
         });
     });
     
@@ -44,11 +42,9 @@ const app = () => {
     const checkPlaying = song => {
         if(song.paused){
             song.play();
-            video.play();
             play.src = "./svg/pause.svg";
         }else{
             song.pause();
-            video.pause();
             play.src = "./svg/play.svg";
         };
     };
